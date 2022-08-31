7f30fabef786c6924797db647a7c60d2eb856bff
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-08-31 18:55:14 -0400

    created elements to display current info

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var appid = '692efab00ae66e9f48137e6ea4766fcd';
 var q = "Chicago";
 //var searchForm = document.querySelector("#searchForm");
 
+//
 var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
 
 fetch(geoURL)
@@ -23,14 +24,25 @@ fetch(geoURL)
       })
       .then(function (data) {
         console.log(data);
+
         var currentEl = document.querySelector("#current");
 
         var h2El = document.createElement('h2');
         var tempEl = document.createElement('p');
+        var windEl = document.createElement('p');
+        var humidityEl = document.createElement('p');
+        var uviEl = document.createElement('p');
         h2El.textContent = city.name;
         tempEl.textContent = 'TEMP: ' + data.current.temp;
+        windEl.textContent = 'WIND: ' + data.current.wind_speed;
+        humidityEl.textContent = 'HUMIDITY: ' + data.current.humidity;
+        uviEl.textContent = 'UV INDEX: ' + data.current.uvi
         currentEl.appendChild(h2El);
         currentEl.appendChild(tempEl);
+        currentEl.appendChild(windEl);
+        currentEl.appendChild(humidityEl);
+        currentEl.appendChild(uviEl);
+        
       });
   });
   searchEl.addEventListener("click", handleSearch);
