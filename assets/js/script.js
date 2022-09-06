da0d7f3d373df0d341809512ce5d8f80580d7d52
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-09-06 18:21:05 -0400

    created functions to search for cities and save to storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,106 @@
 
-var searchBtn = document.querySelector("#search");
+
 var appid = '692efab00ae66e9f48137e6ea4766fcd';
-//var searchForm = document.querySelector('#searchForm');
-var q = 'detroit';
-//var cityInput = document.querySelector("#cityInput");
+
 var storedCities = document.querySelector('#storedCities');
+var searchBtn = document.querySelector("#search");
 
 
 
-var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
+var showWeather = function (data, city) {
+  console.log(data);
+var currentEl = document.querySelector("#current");
+var localDate = new Date(data.current.dt * 1000).toLocaleDateString();
+var h2El = document.createElement('h2');
+var tempEl = document.createElement('p');
+var windEl = document.createElement('p');
+var humidityEl = document.createElement('p');
+var uviEl = document.createElement('p');
+h2El.textContent = city.name + ' ' + localDate;
+tempEl.textContent = 'TEMP: ' + data.current.temp;
+windEl.textContent = 'WIND: ' + data.current.wind_speed;
+humidityEl.textContent = 'HUMIDITY: ' + data.current.humidity;
+uviEl.textContent = 'UV INDEX: ' + data.current.uvi
+currentEl.appendChild(h2El);
+currentEl.appendChild(tempEl);
+currentEl.appendChild(windEl);
+currentEl.appendChild(humidityEl);
+currentEl.appendChild(uviEl);
 
-fetch(geoURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (locations) {
-    var city = locations[0];
-    console.log('LAT', city.lat);
-    console.log('LON', city.lon);
 
-    var oneCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&units=imperial&exclude=hourly,minutely`;
+
+
+var getOneCall = function(city) {
+var oneCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&units=imperial&exclude=hourly,minutely`;
 
     fetch(oneCall)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        showWeather(data, city);
+      })
+    }
+
+    var saveToLocalStorage =function(city) {
+      var cities = JSON.parse(localStorage.getItem('cities')) || [];
+      cities.push(city);
+      var data = JSON.stringify(cities);
+      localStorage.setItem('cities', data)
+    }
+
+    var getGeo = function(locations) {
+      var city = locations[0];
+      console.log('LAT', city.lat);
+      console.log('LON', city.lon);
+      saveToLocalStorage(city.name);
+      getOneCall(city);
+    }
+   
+
+
+var handleSearch = function(event) {
+  event.preventDefault();
+var q = document.querySelector('#q');
+var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q.value}&appid=${appid}`;
+fetch(geoURL)
+  .then(function (response) {
+    return response.json();
+  })
+ 
+    getGeo(locations)
+    
+  }
+  
+  searchBtn.addEventListener('click', handleSearch);
+ 
+
+   
+
+    
      
        
-        var currentEl = document.querySelector("#current");
-
-         var localDate = new Date(data.current.dt * 1000).toLocaleDateString();
-         var h2El = document.createElement('h2');
-         var tempEl = document.createElement('p');
-         var windEl = document.createElement('p');
-         var humidityEl = document.createElement('p');
-         var uviEl = document.createElement('p');
-         //currentEl.innerHTML = '';
-         h2El.textContent = city.name + ' ' + localDate;
-         tempEl.textContent = 'TEMP: ' + data.current.temp;
-         windEl.textContent = 'WIND: ' + data.current.wind_speed;
-         humidityEl.textContent = 'HUMIDITY: ' + data.current.humidity;
-         uviEl.textContent = 'UV INDEX: ' + data.current.uvi
-         currentEl.appendChild(h2El);
-         currentEl.appendChild(tempEl);
-         currentEl.appendChild(windEl);
-         currentEl.appendChild(humidityEl);
-         currentEl.appendChild(uviEl);
-
+       
         
 
-         var fiveForecast = function(data){
-          for(var i = 0; i < 5; i++) {
-        console.log(data.daily[i])
-        forecastDate[i].textContent = new Date(data.daily[i].dt * 1000).toLocaleDateString()
-        var h5El = document.createElement('h5');
-        temp[i].innerHTML = 'TEMP:'
-        tempEl.textContent = 'TEMP: ' + data.daily[i].temp.day;
-        windEl.textContent = 'WIND: ' + data.daily[i].wind_speed;
-        humidityEl.textContent = 'HUMIDITY: ' + data.daily[i].humidity;
-        cardEl.appendChild(h5El);
-        cardEl.appendChild(tempEl);
-        cardEl.appendChild(windEl);
-        cardEl.appendChild(humidityEl);
+      //   var fiveForecast = function(data){
+      //    for(var i = 0; i < 5; i++) {
+      //  console.log(data.daily[i])
+      //  forecastDate[i].textContent = new Date(data.daily[i].dt * 1000).toLocaleDateString()
+      //  var h5El = document.createElement('h5');
+      //  temp[i].innerHTML = 'TEMP:'
+      //  tempEl.textContent = 'TEMP: ' + data.daily[i].temp.day;
+      //  windEl.textContent = 'WIND: ' + data.daily[i].wind_speed;
+      //  humidityEl.textContent = 'HUMIDITY: ' + data.daily[i].humidity;
+      //  cardEl.appendChild(h5El);
+      //  cardEl.appendChild(tempEl);
+      //  cardEl.appendChild(windEl);
+      //  cardEl.appendChild(humidityEl);
 
-        }
-     };
-   });
-   
-  })
+  
+ 
+
+  
 
 
 
